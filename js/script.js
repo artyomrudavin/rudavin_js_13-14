a5c3090c72e4fddf397cefc5cd12a8f73226ca9a
Artyom Rudavin <arrudavin7@gmail.com>
2017-05-29 18:11:35 +0300

    added show and hide result functions

--- js/script.js
+++ js/script.js
@@ -2,4 +2,29 @@ $(function() {
 	'use strict';
 
 
+	var $body = $('body');
+	var $result_btn = $('.result');
+	var $reset_btn = $('.reset');
+	var $modal = $('.modal');
+	
+
+	$result_btn.on('click', showResult);
+	
+	$reset_btn.on('click', reloadResult);
+
+	function showResult() {
+
+		$modal.toggleClass('hide');
+		$modal.one('click', function() {
+			$(this).toggleClass('hide');
+		});
+		
+	};	
+
+	function reloadResult() {
+
+		location.reload();
+
+	};
+
 });
